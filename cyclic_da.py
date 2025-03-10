c96941e4915b633a2053602b393697f9f5996ad8
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-03-10 09:05:54 +0700

    fix: update constants

--- cyclic_da.py
+++ cyclic_da.py
@@ -5,6 +5,7 @@ import time
 
 import minio
 import numpy as np
+import onnxruntime
 import pandas as pd
 import torch
 import torch.optim as optim
@@ -17,6 +18,9 @@ torch.cuda.empty_cache()
 
 AWS_S3_ENDPOINT_URL = "localhost:3900"
 BUCKET_NAME = "era-bucket"
+ONNX_MODEL_PATH = "/content/drive/MyDrive/model.onnx"
+BASE_DATA_DIR = "/content/drive/MyDrive/data/content/data"
+GCLOUD_BUCKET = "gs://gcp-public-data-arco-era5/ar/1959-2022-6h-1440x721.zarr"
 
 
 env.read_env()
@@ -27,12 +31,12 @@ def arg_parser():
     parser.add_argument(
         "--start_time",
         type=str,
-        default="2019-01-03 00:00:00",
+        default="2018-01-01 00:00:00",
     )
     parser.add_argument(
         "--end_time",
         type=str,
-        default="2019-01-10 23:00:00",
+        default="2018-02-20 23:00:00",
     )
     parser.add_argument(
         "--coeff_dir",
@@ -100,8 +104,8 @@ class data_reader:
     def __init__(self, obs_type, obs_std, model_std, da_win, cycle_time, step_int_time):
         self.client = minio.Minio(
             AWS_S3_ENDPOINT_URL,
-            env("AWS_ACCESS_KEY_ID"),
-            env("AWS_SECRET_ACCESS_KEY"),
+            env("AWS_ACCESS_KEY_ID", ""),
+            env("AWS_SECRET_ACCESS_KEY", ""),
             # Force the region, this is specific to garage
             region="garage",
             secure=False,
@@ -122,9 +126,7 @@ class data_reader:
         if save_timestamp:
             self.timestamp = tstamp
 
-        ds = xr.open_zarr(
-            "gs://gcp-public-data-arco-era5/ar/1959-2022-6h-1440x721.zarr"
-        )
+        ds = xr.open_zarr(GCLOUD_BUCKET)
 
         selected_levels = [
             50,
@@ -189,7 +191,10 @@ class data_reader:
         return torch.from_numpy(np.array(res)).to(self.device)
 
     def get_one_state_from_local(
-        self, tstamp, base_dir="./dist/data", save_timestamp=True
+        self,
+        tstamp,
+        base_dir=BASE_DATA_DIR,
+        save_timestamp=True,
     ):
         if save_timestamp:
             self.timestamp = tstamp
@@ -241,7 +246,7 @@ class data_reader:
         return torch.from_numpy(state).to(self.device)
 
     def get_state(self, tstamp):
-        return self.get_one_state_from_local(tstamp)
+        return self.get_one_state_from_gcloud(tstamp)
 
     def get_obs_mask(self, tstamp):
         H = torch.zeros(self.da_win, 69, 721, 1440).to(self.device)
@@ -381,12 +386,12 @@ class cyclic_4dvar:
 
     def init_model(self, path):
         # Load ONNX model
-        onnx_model_path = "model.onnx"
-        # model = onnxruntime.InferenceSession(
-        #     onnx_model_path, providers=["CPUExecutionProvider"]
-        # )
+        onnx_model_path = ONNX_MODEL_PATH
+        model = onnxruntime.InferenceSession(
+            onnx_model_path, providers=["CPUExecutionProvider"]
+        )
 
-        return None
+        return model
 
     def init_file_dir(self):
         os.makedirs(f"da_cycle_results/{self.name}", exist_ok=True)
@@ -480,10 +485,12 @@ class cyclic_4dvar:
         # output_name = model.get_outputs()[0].name  # Get output layer name
 
         for _i in range(step):
-            # z = model.run(None, {"input": z})[0]  # Run inference
+            z = model.run(None, {"input": np.reshape(z, (1, 138, 721, 1440))})[
+                0
+            ]  # Run inference
             z = z[: self.nchannel]  # Apply channel slicing as in original code
 
-        z = z[0, :69]
+        z = z.squeeze()[:69]
 
         print(z.shape)
 
